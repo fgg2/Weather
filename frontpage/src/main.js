b55992a99be6f05ab8be4b6559409cb440d73d3b
Adalsteinn95 <Adalsteinn95@gmail.com>
2016-10-26 23:16:31 +0000

    meira api dot

---
+++ frontpage/src/main.js
@@ -0,0 +1,57 @@
+function initAutocomplete() {
+  var pos;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      var geocoder = new google.maps.Geocoder();
+      geocoder.geocode({'location': pos}, function(results, status) {
+          if (status === 'OK') {
+            var location = results[1].formatted_address;
+            putinFrontpage(location);
+          } else {
+            window.alert('Geocoder failed due to: ' + status);
+          }
+        });
+
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  }
+  else {
+    alert('sorry,your broswer doesnt support Geolocation');
+  }
+
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  var markers = [];
+
+
+  searchBox.addListener('places_changed', function() {
+          var input = document.getElementById('pac-input');
+          getgps(input.value);
+        })
+    function getgps(input){
+      console.log(input);
+      var geocoder = new google.maps.Geocoder();
+      geocoder.geocode({
+        'address': input
+      }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          alert("location : " + results[0].geometry.location.lat() + " " + results[0].geometry.location.lng());
+        } else {
+          alert("Something got wrong " + status);
+        }
+    })
+  }
+}
+
+function putinFrontpage(location){
+  var header = document.createElement('h1');
+  header.innerHTML = 'Your current location is ' + location;
+  document.body.appendChild(header);
+
+}
