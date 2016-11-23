840b77e49a6db4a433b9ce6fd65e81209df14d38
Adalsteinn95 <Adalsteinn95@gmail.com>
2016-11-23 23:53:50 +0000

    'lintvillur'

--- committ/src/routes.js
+++ committ/src/routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const fs = require("fs");
+
 const router = new express.Router();
 const api = require('./api');
-var latt = 0;
-var long = 0;
-var address = '';
 
-//database
+let latt = 0;
+let long = 0;
+let address = '';
+
+// database
 const pgp = require('pg-promise')();
 const xss = require('xss');
 
@@ -17,7 +18,7 @@ const db = pgp(DATABASE);
 
 
 router.get('/list', (req, res) => {
-  db.any(`SELECT * FROM location LIMIT 10 OFFSET 0`)
+  db.any('SELECT * FROM location LIMIT 10 OFFSET 0')
     .then(data => {
       // Hér ættum við að senda niðurstöður í template og vinna með HTML þar
       const result = [`<ul>`];
@@ -63,48 +64,40 @@ router.get('/list/:id', (req, res) => {
 
 
 
-//her endar DATABASE
+// her endar DATABASE
 
 router.get('/data', (req, res) => {
-  api.promise(latt,long)
+  api.promise(latt, long)
   .then((result) => {
     res.json(result.data);
   })
   .catch((error) => {
     res.render('error', { title: error, error });
   });
-
 });
 
 router.get('/address', (req, res) => {
-  address = ({address: address});
+  address = ({ address: address });
   res.json(address);
-
-
 });
 
 router.get('/', (req, res) => {
   //res.sendfile('index.html');
   res.render('index');
-
 });
 
 router.post('/', (req, res) => {
-
   latt = xss(req.body.latt);
   long = xss(req.body.long);
   address = xss(req.body.address);
 
-  db.none(`INSERT INTO location (name,latt, long) VALUES ($1, $2, $3)`, [address,latt, long])
+  db.none(`INSERT INTO location (name,latt, long) VALUES ($1, $2, $3)`, [address, latt, long])
     .then(data => {
-      res.render('index', {title: 'hello'});
+      res.render('index', { title: 'hello' });
     })
     .catch(error => {
       res.send(`<p>Gat ekki bætt gögnum við: ${error}</p>`);
     });
-
-
-
 });
 
 
